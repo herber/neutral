3dde413b76651c0560c2681f131c39f9f7bd034b
Tobias Herber <40146720150275@litec.ac.at>
2018-04-02 17:26:09 +0200

    add styled example

---
+++ examples/styles/main.js
@@ -0,0 +1,31 @@
+const neutral = require('../../lib/');
+const html = require('xou');
+const vxv = require('vxv');
+
+const app = neutral();
+
+vxv`
+:global(body) {
+  background: mistyrose;
+}
+`
+
+const homeStyle = vxv`
+  font-family: sans-serif;
+  color: deeppink;
+  text-align: center;
+  margin-top: 40vh;
+
+  & a {
+    color: deeppink;
+  }
+`;
+
+app.route('/', () => {
+  return html`<div class="${ homeStyle }">
+    <h1><a href="https://github.com/herber/neutral">Neutral</a> + <a href="https://github.com/herber/vxv">VXV</a></h1>
+    <p>Neutral handles the content and the functionality - VXV handles the styles.</p>
+  </div>`;
+});
+
+app.mount('body');
