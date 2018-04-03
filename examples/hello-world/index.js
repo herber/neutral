20f06b24bc72528638c74b60f3c1540c436753c0
Tobias Herber <40146720150275@litec.ac.at>
2018-04-03 13:38:42 +0200

    run formatted

--- examples/hello-world/index.js
+++ examples/hello-world/index.js
@@ -1,12 +1,14 @@
 const neutral = require('../../lib/');
 const html = require('xou');
-const app = neutral({ initial: () => ({
-  title: 'Hello world'
-})});
+const app = neutral({
+  initial: () => ({
+    title: 'Hello world'
+  })
+});
 
 app.state.setState({ count: 0 });
 
-app.route('/', (state) => {
+app.route('/', state => {
   const countUp = () => {
     state.setState({ count: app.state.state().count + 1 });
   };
@@ -15,12 +17,12 @@ app.route('/', (state) => {
     <h1>Hello world.</h1>
     <a href="/about">About</a><br>
     <a href="/nope">Not found</a>
-    <p>Count: ${ state.state().count }</p>
-    <button onclick=${ () => countUp() }>+1</button>
+    <p>Count: ${state.state().count}</p>
+    <button onclick=${() => countUp()}>+1</button>
   </div>`;
 });
 
-app.route('/about', (state) => {
+app.route('/about', state => {
   return html`<div>
     <h1>About.</h1>
     <p>This is a single page JavaScript app it runs on the first working version of <a href="https://github.com/herber/neutral">Neutral</a> (a new frontend framework that comes with batteries included).</p>
